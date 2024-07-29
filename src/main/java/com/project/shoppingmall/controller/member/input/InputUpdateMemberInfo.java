0d666e47f33aa65ec4030495cfce9e5134b49ef2
KJungW <sagakkb0707@naver.com>
2024-07-29 12:42:50 +0900

    Fix(Memer): 고침 MemberController API 입력값 검증 버그
    MemberController.updateMemberInfo()의 입력값을 검증하는 검증기능 추가해서
    입력값 검증이 발생하지 않던 버그 수정

--- src/main/java/com/project/shoppingmall/controller/member/input/InputUpdateMemberInfo.java
+++ src/main/java/com/project/shoppingmall/controller/member/input/InputUpdateMemberInfo.java
@@ -4,11 +4,15 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.hibernate.validator.constraints.Length;
 import org.springframework.web.multipart.MultipartFile;
 
 @Getter
 @AllArgsConstructor
 public class InputUpdateMemberInfo {
-  @NotBlank private String nickName;
+  @NotBlank
+  @Length(min = 5, max = 15)
+  private String nickName;
+
   @NotNull private MultipartFile profileImg;
 }
