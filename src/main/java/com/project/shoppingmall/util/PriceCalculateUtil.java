b4b950ab1c427f81c82c70b73a76d3194ef0f544
KJungW <sagakkb0707@naver.com>
2024-08-01 22:08:24 +0900

    Refactor: 변경 PriceCalculateUtil 리펙토링
    PriceCalculateUtil의 메서드 인자타입을 Wrapper클래스에서 기본형으로 변경

--- src/main/java/com/project/shoppingmall/util/PriceCalculateUtil.java
+++ src/main/java/com/project/shoppingmall/util/PriceCalculateUtil.java
@@ -4,15 +4,14 @@ import com.project.shoppingmall.exception.ServerLogicError;
 import java.util.List;
 
 public class PriceCalculateUtil {
-  public static Integer calculatePrice(
-      Integer originPrice, Integer discountAmount, Double discountRate) {
+  public static int calculatePrice(int originPrice, int discountAmount, Double discountRate) {
     if (originPrice <= 0 || discountAmount < 0 || discountRate < 0) {
       throw new ServerLogicError("잘못된 가격계산 입력값입니다.");
     }
     return originPrice - discountAmount - ((int) (originPrice * discountRate / 100));
   }
 
-  public static Integer addOptionPrice(Integer originPrice, List<Integer> optionPrices) {
+  public static int addOptionPrice(int originPrice, List<Integer> optionPrices) {
     if (originPrice <= 0 || optionPrices == null) {
       throw new ServerLogicError("잘못된 가격계산 입력값입니다.");
     }
