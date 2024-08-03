d28bfb53b22936761b3d5761394a5b0db25e605d
KJungW <sagakkb0707@naver.com>
2024-08-03 19:43:25 +0900

    Refactor: 변경 ErrorResult의 생성자
    ErrorResult를 JSON으로 인코딩/디코딩 하기위해 인자없는 생성자를 추가

--- src/main/java/com/project/shoppingmall/dto/exception/ErrorResult.java
+++ src/main/java/com/project/shoppingmall/dto/exception/ErrorResult.java
@@ -3,10 +3,12 @@ package com.project.shoppingmall.dto.exception;
 import com.project.shoppingmall.type.ErrorCode;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 @AllArgsConstructor
 public class ErrorResult {
-  ErrorCode Code;
+  ErrorCode code;
   String message;
 }
