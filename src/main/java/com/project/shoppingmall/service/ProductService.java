e58e9519b00faaaf860014e8ab35b3adf61e2c27
KJungW <sagakkb0707@naver.com>
2024-08-04 21:41:23 +0900

    Refactor: 변경 ProductService 메서드 정리
    사용하지 않는 메서드 제거
    불필요하게 공개된 로직 메서드를 private으로 변경

--- src/main/java/com/project/shoppingmall/service/ProductService.java
+++ src/main/java/com/project/shoppingmall/service/ProductService.java
@@ -129,19 +129,7 @@ public class ProductService {
     }
   }
 
-  public Optional<Product> findByIdWithOption(Long productId) {
-    Optional<Product> result = findById(productId);
-    if (result.isPresent()) {
-      Product product = result.get();
-      int singleOptionSize = product.getSingleOptions().size();
-      int multiOptionSize = product.getMultipleOptions().size();
-      return Optional.of(product);
-    } else {
-      return Optional.empty();
-    }
-  }
-
-  public boolean validateMemberIsProductSeller(Product product, Long memberId) {
+  private boolean validateMemberIsProductSeller(Product product, Long memberId) {
     return product.getSeller().getId().equals(memberId);
   }
 
