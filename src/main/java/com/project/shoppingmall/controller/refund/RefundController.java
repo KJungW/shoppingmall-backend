2f238595e87aaddb80436b2fe92d8b769bfee348
KJungW <sagakkb0707@naver.com>
2024-08-09 12:29:57 +0900

    Refactor: 고침 RefundService.requestRefund() 리턴값
    새로 저장된 Refund의 ID값이 아닌 Refudn자체를 리턴하도록 변경

--- src/main/java/com/project/shoppingmall/controller/refund/RefundController.java
+++ src/main/java/com/project/shoppingmall/controller/refund/RefundController.java
@@ -3,6 +3,7 @@ package com.project.shoppingmall.controller.refund;
 import com.project.shoppingmall.controller.refund.input.InputRequestRefund;
 import com.project.shoppingmall.controller.refund.output.OutputRequestRefund;
 import com.project.shoppingmall.dto.auth.AuthUserDetail;
+import com.project.shoppingmall.entity.Refund;
 import com.project.shoppingmall.service.RefundService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -22,12 +23,12 @@ public class RefundController {
   public OutputRequestRefund requestRefund(@Valid @RequestBody InputRequestRefund input) {
     AuthUserDetail userDetail =
         (AuthUserDetail) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-    long newRefundId =
+    Refund newRefund =
         refundService.saveRefund(
             userDetail.getId(),
             input.getPurchaseItemId(),
             input.getRequestTitle(),
             input.getRequestContent());
-    return new OutputRequestRefund(newRefundId);
+    return new OutputRequestRefund(newRefund.getId());
   }
 }
--- src/main/java/com/project/shoppingmall/service/RefundService.java
+++ src/main/java/com/project/shoppingmall/service/RefundService.java
@@ -20,7 +20,7 @@ public class RefundService {
   private final PurchaseItemService purchaseItemService;
 
   @Transactional
-  public long saveRefund(
+  public Refund saveRefund(
       Long memberId, long purchaseItemId, String requestTitle, String requestContent) {
     Member member =
         memberService
@@ -31,11 +31,11 @@ public class RefundService {
             .findByIdWithPurchaseAndRefund(purchaseItemId)
             .orElseThrow(() -> new DataNotFound("Id에 해당하는 구매아이템이 존재하지 않습니다."));
 
-    if (!purchaseItem.getPurchase().getState().equals(PurchaseStateType.COMPLETE)) {
-      throw new DataNotFound("결제가 되지 않은 구매에 대해 환불은 불가능합니다.");
-    }
     if (!purchaseItem.getPurchase().getBuyer().getId().equals(member.getId())) {
       throw new DataNotFound("현재 회원의 구매 아이템이 아닙니다.");
+    }
+    if (!purchaseItem.getPurchase().getState().equals(PurchaseStateType.COMPLETE)) {
+      throw new DataNotFound("결제가 되지 않은 구매에 대해 환불은 불가능합니다.");
     }
     if (!purchaseItemService.refundIsPossible(purchaseItem)) {
       throw new ProcessOrCompleteRefund("이미 진행중이거나 완료된 환불이 존재합니다.");
@@ -51,6 +51,6 @@ public class RefundService {
 
     refundRepository.save(newRefund);
 
-    return newRefund.getId();
+    return newRefund;
   }
 }
