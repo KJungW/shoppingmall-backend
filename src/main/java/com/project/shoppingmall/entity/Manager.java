a71cb660da8798030033892c6f347ad285141bb0
KJungW <sagakkb0707@naver.com>
2024-08-19 14:53:14 +0900

    !BREAKING_CHANCE: 추가 Manager 관련 엔티티
    Manager엔티티 추가
    ManagerToken 엔티티 추가

---
+++ src/main/java/com/project/shoppingmall/entity/Manager.java
@@ -0,0 +1,40 @@
+package com.project.shoppingmall.entity;
+
+import com.project.shoppingmall.exception.ServerLogicError;
+import com.project.shoppingmall.type.ManagerRoleType;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Manager extends BaseEntity {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private String serialNumber;
+  private String password;
+
+  @Enumerated(EnumType.STRING)
+  private ManagerRoleType role;
+
+  @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
+  @JoinColumn(name = "TOKEN_ID")
+  private ManagerToken token;
+
+  @Builder
+  public Manager(String serialNumber, String password, ManagerRoleType role) {
+    if (serialNumber == null || serialNumber.isBlank())
+      throw new ServerLogicError("Manager를 생성할때 비어있는 serialNumber값을 입력했습니다.");
+    if (password == null || password.isBlank())
+      throw new ServerLogicError("Manager를 생성할때 비어있는 password값을 입력했습니다.");
+    if (role == null) throw new ServerLogicError("Manager를 생성할때 비어있는 role값을 입력했습니다.");
+    this.serialNumber = serialNumber;
+    this.password = password;
+    this.role = role;
+  }
+}
---
+++ src/main/java/com/project/shoppingmall/type/ManagerRoleType.java
@@ -0,0 +1,6 @@
+package com.project.shoppingmall.type;
+
+public enum ManagerRoleType {
+  ROLE_ROOT_MANAGER,
+  ROLE_COMMON_MANGER
+}
