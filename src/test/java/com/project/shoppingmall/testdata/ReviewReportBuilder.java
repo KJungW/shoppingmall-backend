1310cd316b69ff3987d2774065b63525f6bac260
KJungW <sagakkb0707@naver.com>
2024-08-20 17:07:02 +0900

    Test: 고침 ReviewReportBuilder 데이터 구성
    ReviewReport.reporter 필드에 비어있는 Member가 삽입되는 버그 수정

--- src/test/java/com/project/shoppingmall/testdata/ReviewReportBuilder.java
+++ src/test/java/com/project/shoppingmall/testdata/ReviewReportBuilder.java
@@ -1,13 +1,12 @@
 package com.project.shoppingmall.testdata;
 
-import com.project.shoppingmall.entity.Member;
 import com.project.shoppingmall.entity.report.ReviewReport;
 import java.io.IOException;
 
 public class ReviewReportBuilder {
   public static ReviewReport.ReviewReportBuilder fullData() throws IOException {
     return ReviewReport.builder()
-        .reporter(Member.builder().build())
+        .reporter(MemberBuilder.fullData().build())
         .title("test title")
         .description("test description")
         .review(ReviewBuilder.fullData().build());
