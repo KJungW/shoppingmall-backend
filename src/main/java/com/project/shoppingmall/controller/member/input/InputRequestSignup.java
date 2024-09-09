3b7cbdb4927c0476e203b699c8e6276d29c186dd
KJungW <sagakkb0707@naver.com>
2024-09-09 12:14:25 +0900

    Feat(Member): 변경 이메일 회원가입 요청 API 입력값 검증로직
    비밀번호 입력값에 ^(?=.*[A-Za-z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,15}$ 정규표현식 검증추가

--- src/main/java/com/project/shoppingmall/controller/member/input/InputRequestSignup.java
+++ src/main/java/com/project/shoppingmall/controller/member/input/InputRequestSignup.java
@@ -1,7 +1,9 @@
 package com.project.shoppingmall.controller.member.input;
 
+import com.project.shoppingmall.final_value.RegularExpressions;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.hibernate.validator.constraints.Length;
@@ -10,7 +12,10 @@ import org.hibernate.validator.constraints.Length;
 @AllArgsConstructor
 public class InputRequestSignup {
   @NotBlank @Email private String email;
-  @NotBlank private String password;
+
+  @NotBlank
+  @Pattern(regexp = RegularExpressions.MEMBER_PASSWORD_PATTERN)
+  private String password;
 
   @NotBlank
   @Length(min = 3, max = 30)
--- src/main/java/com/project/shoppingmall/final_value/RegularExpressions.java
+++ src/main/java/com/project/shoppingmall/final_value/RegularExpressions.java
@@ -2,4 +2,6 @@ package com.project.shoppingmall.final_value;
 
 public class RegularExpressions {
   public static final String PRODUCT_TYPE_PATTERN = "[가-힣a-zA-Z0-9 \\-]+\\$[가-힣a-zA-Z0-9 \\-]+";
+  public static final String MEMBER_PASSWORD_PATTERN =
+      "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[!@#$%^&*])[A-Za-z\\d!@#$%^&*]{8,15}$";
 }
